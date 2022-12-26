fb82e36e64724aa8b104701cb59070bad2648683
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2022-12-25 19:52:25 -0500

    Removed adjacency list, instead implementing directly as Graph

--- testing/data_structures/adjacency_list_tests.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef ADJACENCY_LIST_TESTS_HPP
-#define ADJACENCY_LIST_TESTS_HPP
-
-#include "test_utils/test_manager.hpp"
-
-void adjacencyListTestRegisterTests(TestManager* test_manager);
-
-#endif
