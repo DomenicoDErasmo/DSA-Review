b53a21f34c29ba23151282c1db598defe383be7c
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2022-12-29 15:25:44 -0500

    Added getNode, addNode, addEdge

--- src/data_structures/graph.cpp
+++ src/data_structures/graph.cpp
@@ -8,16 +8,16 @@ template <typename T>
 struct Graph {
 public:
     // Fields
-    LinkedList<AdjacencyList<T>>* adjacency_list;
+    LinkedList<AdjacencyList<T>>* adjacency_matrix;
 
     // Constructors
-    Graph(): adjacency_list(nullptr) {}
+    Graph(): adjacency_matrix(nullptr) {}
     Graph(const Graph<T>& other): 
-        adjacency_list(new LinkedList<AdjacencyList<T>>(
-            *other.adjacency_list)) {}
+        adjacency_matrix(new LinkedList<AdjacencyList<T>>(
+            *other.adjacency_matrix)) {}
 
     // Destructor
-    ~Graph() {delete adjacency_list;}
+    ~Graph() {delete adjacency_matrix;}
 
     // Operators
 
@@ -34,19 +34,13 @@ public:
 
     /**
      * @brief Equality operator for graphs.
-     * Because the adjacency list only checks for the same "from", this isn't
-     * a full equality check
-     * 
-     * TODO: revert edge and adjacency list equality operators
-     * TODO: make a "loose equality" function mirroring the current version
-     * TODO: change linked list insertions to take a function, default of ==?
      * 
      * @param lhs The left graph to check
      * @param rhs The right graph to check
      * @return true if the graphs are equal, otherwise false
      */
     friend bool operator == (const Graph<T>& lhs, const Graph<T>& rhs) {
-        return *lhs.adjacency_list == *rhs.adjacency_list;
+        return *lhs.adjacency_matrix == *rhs.adjacency_matrix;
     }
 
     /**
@@ -57,7 +51,7 @@ public:
      * @return true if the graphs are inequal, otherwise false
      */
     friend bool operator != (const Graph<T>& lhs, const Graph<T>& rhs) {
-        return *lhs.adjacency_list == *rhs.adjacency_list;
+        return !(*lhs.adjacency_matrix == *rhs.adjacency_matrix);
     }
 
     // Utility Functions
@@ -70,14 +64,82 @@ public:
     friend void swap(Graph<T>& lhs, Graph<T>& rhs) {
         using std::swap;
 
-        swap(lhs.adjacency_list, rhs.adjacency_list);
+        swap(lhs.adjacency_matrix, rhs.adjacency_matrix);
     }
 };
 
-// TODO: add node, add edge, many other ops
+/**
+ * @brief Gets the adjacency list in the graph
+ * 
+ * @tparam T The type of the graph's data
+ * @param graph The graph to search
+ * @param data The node to search for in the graph
+ * @return LinkedList<AdjacencyList<T>>* The node if it exists
+ */
+template <typename T>
+LinkedList<AdjacencyList<T>>* graphGetNode(Graph<T>& graph, T data) {
+    return linkedListGetNthOccurrence(
+        graph.adjacency_matrix, 
+        AdjacencyList<T>(data), 
+        1);
+}
+
+/**
+ * @brief Checks if the graph has the given node
+ * 
+ * @tparam T The type of the graph's data
+ * @param graph The graph to search
+ * @param data The node to search for in the graph
+ * @return true if the node is found, otherwise false
+ */
+template <typename T>
+bool graphHasNode(Graph<T>& graph, T data) {
+    return graphGetNode(graph, data) != nullptr;
+}
+
+/**
+ * @brief Adds an adjacency list to the graph if none exists
+ * 
+ * @tparam T The type of the graph's data
+ * @param graph The graph to add an adjacency list to
+ * @param data The data to add to the graph
+ */
 template <typename T>
 void graphAddNode(Graph<T>& graph, T data) {
-    
+    if (!graphHasNode(graph, data)) {
+        linkedListInsertAtTail(&graph.adjacency_matrix, AdjacencyList<T>(data));
+    }
 }
+
+/**
+ * @brief Adds an edge to the graph
+ * 
+ * @tparam T 
+ * @param graph 
+ * @param edge 
+ */
+template <typename T>
+void graphAddEdge(Graph<T>& graph, Edge<T> edge) {
+    LinkedList<AdjacencyList<T>>* adj_list = graphGetNode(graph, edge.from);
+    if (!adj_list) {
+        throw std::logic_error("Can't add an edge if the graph doesn't exist.");
+    }
+
+    LinkedList<Edge<T>>* found = linkedListGetNthOccurrence(
+        adj_list->data.edges, 
+        edge,
+        1);
+
+    if (found != nullptr) {
+        throw std::logic_error(
+            "Can't add an edge if the edge already exists."
+            "Instead use graphUpdateNode.");
+    }
+
+    linkedListInsertAtTail(&adj_list->data.edges, edge);
+}
+
+// TODO: add many other ops - CRUD for adjacency lists and edges
+
 
 #endif
--- testing/data_structures/graph_tests.cpp
+++ testing/data_structures/graph_tests.cpp
@@ -5,7 +5,7 @@ bool graphTestDefaultConstructor() {
     bool result = true;
 
     Graph<int> empty;
-    result &= !empty.adjacency_list;
+    result &= !empty.adjacency_matrix;
 
     return result;
 }
@@ -16,19 +16,19 @@ bool graphTestCopyConstructor() {
     bool result = true;
 
     Graph<int> original;
-    linkedListInsertAtTail(&original.adjacency_list, AdjacencyList<int>(4));
-    linkedListInsertAtTail(&original.adjacency_list, AdjacencyList<int>(5));
+    linkedListInsertAtTail(&original.adjacency_matrix, AdjacencyList<int>(4));
+    linkedListInsertAtTail(&original.adjacency_matrix, AdjacencyList<int>(5));
     adjacencyListAddEdge(
         linkedListGetNthOccurrence(
-            original.adjacency_list, AdjacencyList<int>(4), 1)->data,
+            original.adjacency_matrix, AdjacencyList<int>(4), 1)->data,
         Edge<int>(5, 4));
     Graph<int> copy(original);
     LinkedList<AdjacencyList<int>>* original_adj_node = 
         linkedListGetNthOccurrence(
-            original.adjacency_list, AdjacencyList<int>(5), 1);
+            original.adjacency_matrix, AdjacencyList<int>(5), 1);
     LinkedList<AdjacencyList<int>>* copy_adj_node = 
         linkedListGetNthOccurrence(
-            copy.adjacency_list, AdjacencyList<int>(5), 1);
+            copy.adjacency_matrix, AdjacencyList<int>(5), 1);
     result &= *copy_adj_node->data.edges == *original_adj_node->data.edges;
 
     return result;
@@ -38,19 +38,19 @@ bool graphTestAssignmentOperator() {
     bool result = true;
 
     Graph<int> original;
-    linkedListInsertAtTail(&original.adjacency_list, AdjacencyList<int>(4));
-    linkedListInsertAtTail(&original.adjacency_list, AdjacencyList<int>(5));
+    linkedListInsertAtTail(&original.adjacency_matrix, AdjacencyList<int>(4));
+    linkedListInsertAtTail(&original.adjacency_matrix, AdjacencyList<int>(5));
     adjacencyListAddEdge(
         linkedListGetNthOccurrence(
-            original.adjacency_list, AdjacencyList<int>(4), 1)->data,
+            original.adjacency_matrix, AdjacencyList<int>(4), 1)->data,
         Edge<int>(5, 4));
     Graph<int> copy = original;
     LinkedList<AdjacencyList<int>>* original_adj_node = 
         linkedListGetNthOccurrence(
-            original.adjacency_list, AdjacencyList<int>(5), 1);
+            original.adjacency_matrix, AdjacencyList<int>(5), 1);
     LinkedList<AdjacencyList<int>>* copy_adj_node = 
         linkedListGetNthOccurrence(
-            copy.adjacency_list, AdjacencyList<int>(5), 1);
+            copy.adjacency_matrix, AdjacencyList<int>(5), 1);
     result &= *copy_adj_node->data.edges == *original_adj_node->data.edges;
 
     return result;
@@ -58,6 +58,138 @@ bool graphTestAssignmentOperator() {
 
 bool graphTestEqualityOperator() {
     bool result = true;
+
+    Graph<int> inequal_lhs;
+    linkedListInsertAtTail(
+        &inequal_lhs.adjacency_matrix, 
+        AdjacencyList<int>(4));
+    linkedListInsertAtTail(
+        &inequal_lhs.adjacency_matrix, 
+        AdjacencyList<int>(5));
+    adjacencyListAddEdge(
+        linkedListGetNthOccurrence(
+            inequal_lhs.adjacency_matrix, 
+            AdjacencyList<int>(4), 
+            1)->data,
+        Edge<int>(5, 4));
+    Graph<int> inequal_rhs;
+    linkedListInsertAtTail(
+        &inequal_rhs.adjacency_matrix, 
+        AdjacencyList<int>(4));
+    linkedListInsertAtTail(
+        &inequal_rhs.adjacency_matrix, 
+        AdjacencyList<int>(5));
+    adjacencyListAddEdge(
+        linkedListGetNthOccurrence(
+            inequal_rhs.adjacency_matrix, 
+            AdjacencyList<int>(4), 
+            1)->data,
+        Edge<int>(5, 4, 1.1));
+    result &= !(inequal_lhs == inequal_rhs);
+
+    Graph<int> equal_lhs;
+    linkedListInsertAtTail(
+        &equal_lhs.adjacency_matrix, 
+        AdjacencyList<int>(4));
+    linkedListInsertAtTail(
+        &equal_lhs.adjacency_matrix, 
+        AdjacencyList<int>(5));
+    adjacencyListAddEdge(
+        linkedListGetNthOccurrence(
+            equal_lhs.adjacency_matrix, 
+            AdjacencyList<int>(4), 
+            1)->data,
+        Edge<int>(5, 4));
+    Graph<int> equal_rhs;
+    linkedListInsertAtTail(&equal_rhs.adjacency_matrix, AdjacencyList<int>(4));
+    linkedListInsertAtTail(&equal_rhs.adjacency_matrix, AdjacencyList<int>(5));
+    adjacencyListAddEdge(
+        linkedListGetNthOccurrence(
+            equal_rhs.adjacency_matrix, AdjacencyList<int>(4), 1)->data,
+        Edge<int>(5, 4));
+    result &= equal_lhs == equal_rhs;
+    return result;
+}
+
+bool graphTestGetNode() {
+    bool result = true;
+
+    Graph<int> node_not_found;
+    result &= !graphGetNode(node_not_found, 4);
+
+    Graph<int> node_found;
+    linkedListInsertAtTail(&node_found.adjacency_matrix, AdjacencyList<int>(5));
+    LinkedList<AdjacencyList<int>>* list = graphGetNode(node_found, 5);
+    result &= list->data.from == 5;
+
+    return result;
+}
+
+bool graphTestHasNode() {
+    bool result = true;
+
+    Graph<int> node_not_found;
+    result &= !graphHasNode(node_not_found, 5);
+
+    Graph<int> node_found;
+    result &= !node_found.adjacency_matrix;
+    linkedListInsertAtTail(
+        &node_found.adjacency_matrix, 
+        AdjacencyList<int>(5));
+    result &= graphHasNode(node_found, 5);
+
+    return result;
+}
+
+bool graphTestAddNode() {
+    bool result = true;
+
+    Graph<int> can_insert;
+    result &= !can_insert.adjacency_matrix;
+    graphAddNode(can_insert, 4);
+    result &= can_insert.adjacency_matrix->data == 4;
+
+    Graph<int> already_found;
+    result &= !already_found.adjacency_matrix;
+    linkedListInsertAtTail(
+        &already_found.adjacency_matrix, 
+        AdjacencyList<int>(5));
+    result &= linkedListGetLength(already_found.adjacency_matrix) == 1;
+    graphAddNode(already_found, 5);
+    result &= linkedListGetLength(already_found.adjacency_matrix) == 1;
+
+    return result;
+}
+
+bool graphTestAddEdge() {
+    bool result = true;
+
+    Graph<int> adj_list_not_found;
+    try {
+        graphAddEdge(adj_list_not_found, Edge<int>(4, 3));
+    } catch (std::logic_error) {
+        result &= true;
+    } catch(...) {
+        result &= false;
+    }
+
+    Graph<int> edge_found;
+    graphAddNode(edge_found, 4);
+    linkedListInsertAtTail(
+        &edge_found.adjacency_matrix->data.edges, 
+        Edge<int>(4, 3));
+    try {
+        graphAddEdge(edge_found, Edge<int>(4, 3));
+    } catch (std::logic_error) {
+        result &= true;
+    } catch(...) {
+        result &= false;
+    }
+
+    Graph<int> found;
+    graphAddNode(found, 4);
+    graphAddEdge(found, Edge<int>(4, 5, 1.2));
+    result &= found.adjacency_matrix->data.edges->data == Edge<int>(4, 5, 1.2);
 
     return result;
 }
@@ -73,6 +205,10 @@ void graphTestRegisterTests(TestManager* test_manager) {
         graphTestAssignmentOperator));
     testGroupAddTest(&test_group, UnitTest("equality operator", 
         graphTestEqualityOperator));
+    testGroupAddTest(&test_group, UnitTest("get node", graphTestGetNode));
+    testGroupAddTest(&test_group, UnitTest("has node", graphTestHasNode));
+    testGroupAddTest(&test_group, UnitTest("add node", graphTestAddNode));
+    testGroupAddTest(&test_group, UnitTest("add edge", graphTestAddEdge));
 
     testManagerAddTestGroup(test_manager, test_group);
 }
